2f150996c543240744f72c5e715e189031aa48cd
KuuLotus <kuulotusy@gmail.com>
2023-07-20 21:53:57 +0900

    [finish] InfoModal

--- components/InfoModal.tsx
+++ components/InfoModal.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { AiOutlineClose } from "react-icons/ai";
 
 import PlayButton from "./PlayButton";
@@ -17,8 +17,63 @@ const InfoModal: React.FC<useInfoModalProps> = ({ visible, onClose }) => {
   const { movieId } = useInfoModal();
   const { data = {} } = useMovie(movieId);
 
-  useEffect(() => {}, []);
-  return <div>InfoModal</div>;
+  useEffect(() => {
+    setIsVisible(!!visible);
+  }, [visible]);
+
+  const handleClose = useCallback(() => {
+    setIsVisible(false);
+    setTimeout(() => {
+      onClose();
+    }, 300);
+  }, [onClose]);
+
+  if (!visible) {
+    return null;
+  }
+  return (
+    <div className="z-50 transition duration-300 bg-black bg-opacity-80 flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0">
+      <div className="relative w-auto mx-auto max-w-3xl rounded-md overflow-hidden">
+        <div
+          className={`${
+            isVisible ? "scale-100" : "scale-0"
+          } transform duration-300 relative flex-auto bg-zinc-900 drop-shadow-md`}
+        >
+          <div className="relative h-96">
+            <video
+              className="w-full brightness-[60%] object-cover h-full"
+              src={data?.videoUrl}
+              poster={data.thumbnailUrl}
+              loop
+              muted
+              autoPlay
+            ></video>
+            <div
+              className="cursor-pointer absolute top-3 right-3 w-10 h-10 rounded-full bg-black bg-opacity-70 flex items-center justify-center"
+              onClick={handleClose}
+            >
+              <AiOutlineClose className="text-white" size={20} />
+            </div>
+            <div className="absolute bottom-[10%] left-10">
+              <p className="text-white text-3xl md:text-4xl h-full lg:text-5xl font-bold mb-8">
+                {data?.title}
+              </p>
+              <div className="flex flex-row gap-4 items-center">
+                <PlayButton movieId={data?.id} />
+                <FavoriteButton movieId={data?.id} />
+              </div>
+            </div>
+          </div>
+          <div className="px-12 py-8">
+            <p className="text-green-400 font-semibold text-lg">New</p>
+            <p className="text-white text-lg">{data?.duration}</p>
+            <p className="text-white text-lg">{data?.genre}</p>
+            <p className="text-white text-lg">{data?.description}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default InfoModal;
